8b7b794e623bbc9292d4ffe113720104105bb19d
nmhummel <natalie.hummel@gmail.com>
2021-02-12 17:37:55 -0600

    commit tree 757d0b14b114f3369883dcb377193edeebbcd7e5

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(sentence) {
+    let minorWords = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let finalArray = [];
+    let wordArray = sentence.split(" ")
+    for (let i= 0; i< wordArray.length; i++) {
+      if (i== 0) {
+        finalArray.push(this.capitalize(wordArray[i]) )
+      } 
+      else {
+        if (minorWords.includes(wordArray[i]) ) {
+          finalArray.push(wordArray[i])
+        } 
+        else {
+          finalArray.push(this.capitalize(wordArray[i]) )
+        }
+      }
+
+    }
+    return finalArray.join(" ");
+  }
 }
+
